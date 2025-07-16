e374767d5b4bf4daf49b11413ec439fc3c9f7582
Daniel Alcocer <christianalma001@gmail.com>
2025-07-15 19:34:21 -0500

    update

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,17 @@
+// src/auth/auth.controller.ts
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
-@Controller('auth')
+@Controller('auth') // <-- Prefijo de controlador 'auth'
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @Post('register')
+  @Post('register') // <-- Ruta '/register' dentro del prefijo 'auth'
   register(@Body() dto: { nombre: string; email: string; password: string }) {
     return this.authService.register(dto);
   }
 
-  @Post('login')
+  @Post('login') // <-- Ruta '/login' dentro del prefijo 'auth'
   login(@Body() dto: { email: string; password: string }) {
     return this.authService.login(dto);
   }
